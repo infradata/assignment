41f81ed8b89d88746487ca9bbe1a811dfd6dd1d0
swathiCD <mohan.swathi8101@gmail.com>
2017-08-20 20:13:28 +0100

    Added few comments

--- exponent.py
+++ exponent.py
@@ -1,3 +1,4 @@
+# this is exponent.py
 base = float(input('Enter a base: '))
 exponent = float(input('Enter an exponent: '))
 print("{0} to the power of {1} is ".format(base,exponent), base**exponent) 
